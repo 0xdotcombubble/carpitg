ee06565dd2c19609bc8eccb69a3d091e8fa2df66
0xdotcombubble <raphael.varszegi@icloud.com>
2025-12-18 17:25:32 +0100

    removed unused import

--- src/components/ui/PricingSection.tsx
+++ src/components/ui/PricingSection.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 import { Check } from 'lucide-react'
 import { PricingItem, SiteSettings } from './types'
 
